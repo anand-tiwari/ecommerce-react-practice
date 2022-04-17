0afd710ed2d8a45786000ddc952d3f41ec8df8d4
anand.tiwari <anand.tiwari@razorpay.com>
2022-04-17 22:20:44 +0530

    listing fix

--- src/RequireAuth.jsx
+++ src/RequireAuth.jsx
@@ -1,7 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useProducts } from "./context/product/context";
 
-export default function RequireAuth({ children}) {
+export default function RequireAuth({ children }) {
   const { productState } = useProducts();
   let location = useLocation()
   return productState.isLoggedIn ? children : <Navigate to="/login" state={{ from: location }} replace />;
